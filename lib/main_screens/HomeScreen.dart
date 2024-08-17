735048eb12cf37479ef9180592dfce3d0f18464d
Schizoid-man <immanuelcant@protonmail.com>
2024-08-17 16:11:50 +0530

    Added HomeScreen icons

--- lib/main_screens/HomeScreen.dart
+++ lib/main_screens/HomeScreen.dart
@@ -1,12 +1,5 @@
-
-import 'dart:math';
-import 'dart:ui';
-
-import 'package:bishop/bishop.dart' as bishop;
-import 'package:chess_app/service/assetsManager.dart';
 import 'package:flutter/material.dart';
-import 'package:square_bishop/square_bishop.dart';
-import 'package:squares/squares.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -16,119 +9,60 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
 
-  late bishop.Game game;
-  late SquaresState state;
-  int player = Squares.white;
-  bool aiThinking = false;
-  bool flipBoard = false;
-
-  @override
-  void initState() {
-    _resetGame(false);
-    super.initState();
-  }
-
-  void _resetGame([bool ss = true]) {
-    game = bishop.Game(variant: bishop.Variant.standard());
-    state = game.squaresState(player);
-    if (ss) setState(() {});
-  }
-
-  void _flipBoard() => setState(() => flipBoard = !flipBoard);
-
-  void _onMove(Move move) async {
-    bool result = game.makeSquaresMove(move);
-    if (result) {
-      setState(() => state = game.squaresState(player));
-    }
-    if (state.state == PlayState.theirTurn && !aiThinking) {
-      setState(() => aiThinking = true);
-      await Future.delayed(
-          Duration(milliseconds: Random().nextInt(4750) + 250));
-      game.makeRandomMove();
-      setState(() {
-        aiThinking = false;
-        state = game.squaresState(player);
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.white,), 
-          onPressed: () {
-            //TODO: show dialog if true
-          Navigator.pop(context);
-        },),
         backgroundColor: Colors.black87,
         title: Text('Flutter Chess', style: TextStyle(color: Colors.amber),),
-        actions: [
-          
-            IconButton(
-              onPressed: _resetGame,
-              icon: const Text('New Game'),
-            ),
-            IconButton(
-              onPressed: _flipBoard,
-              icon: const Icon(Icons.rotate_left, color: Colors.amberAccent,),
-            ),
-        ],
         ),
-        body: Column(
-          //mainAxisAlignment: MainAxisAlignment.center, //add this later
+        body: GridView.count(
+          shrinkWrap: true,
+          crossAxisCount: 2,
           children: [
-            //Oppponent information
-            ListTile(
-              leading: CircleAvatar(
-                radius: 25,
-                backgroundImage: AssetImage(AssetsManager.stockfishIcon),
-                ),
-                title: const Text("Stockfish"),
-                subtitle: const Text("Rating: jalgaara"),
-                trailing: const Text("05:00"),
-              ),
+            buildGameType(
+              label: 'vs Computer', 
+              icon: Icons.computer, 
+              onTap: (){
+                print('player vs computer');
+              }),
+            buildGameType(
+              label: 'vs Player', 
+              icon: Icons.person, 
+              onTap: (){
+                print('player vs player');
+              }),
+              buildGameType(
+              label: 'Settings', 
+              icon: Icons.settings, 
+              onTap: (){
+                print('settings');
+              }),
+              buildGameType(
+              label: 'About', 
+              icon: Icons.info, 
+              onTap: (){
+                print('about');
+              }),
             
+          ],)
         
-        
-        
-        
-        
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: BoardController(
-                state: flipBoard ? state.board.flipped() : state.board,
-                playState: state.state,
-                pieceSet: PieceSet.merida(),
-                theme: BoardTheme.brown,
-                moves: state.moves,
-                onMove: _onMove,
-                onPremove: _onMove,
-                markerTheme: MarkerTheme(
-                  empty: MarkerTheme.dot,
-                  piece: MarkerTheme.corners(),
-                ),
-                promotionBehaviour: PromotionBehaviour.autoPremove,
-              ),
-            ),
-        
-            //Player information
-            ListTile(
-              leading: CircleAvatar(
-                radius: 25,
-                backgroundImage: AssetImage(AssetsManager.userIcon),
-                ),
-                title: const Text("Edging Lord"),
-                subtitle: const Text("Rating: 5000"),
-                trailing: const Text("05:00"),
-              ),
-        
-        
-            
-          ],
-        ),
     );
   }
+}
+
+Widget buildGameType({required label, String? gameTime, IconData? icon, required Function() onTap}){
+  return InkWell(
+    onTap: onTap,
+    child: Card(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    icon !=null ? Icon(icon): Text(gameTime!),
+                    const SizedBox(height: 10,),
+                    Text(label, style: TextStyle(fontWeight: FontWeight.bold),)
+                  ],
+                ),
+              ),
+  );
 }
